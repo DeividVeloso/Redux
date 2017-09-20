f36cd21811df763cf96ad27fe0324146f903b42f
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-09-20 09:14:37 -0300

    refactoring const in Actions and Reducers

--- pluralsight-redux-starter/src/components/course/CoursesPage.js
+++ pluralsight-redux-starter/src/components/course/CoursesPage.js
@@ -1,51 +1,57 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.state = { course: { title: '' } };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
+    this.state = { course: { title: '' } };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
 
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
 
-    onClickSave(){
-        //this.props.dispatch(courseActions.createCourse(this.state.course));
-        this.props.createCourse(this.state.course);
-    }
+  onClickSave() {
+    //this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.actions.createCourse(this.state.course);
+  }
 
-    courseRow(course, index){
-        return <div key={index}>{course.title}</div>;
-    }
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Courses</h1>
-                <h2>Add Course</h2>
-                {this.props.courses.map((course, index) => this.courseRow(course,index))}
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}
-                />
-                <input
-                    type="submit"
-                    onClick={this.onClickSave}
-                    value="Save"
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        {this.props.courses.map((course, index) => this.courseRow(course, index))}
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <input
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save"
+        />
+      </div>
+    );
+  }
 }
+
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
 
 /*
 Essa função retorna as props que queremos expor no componente. São props que vem dos reducers que vem do Redux Store.
@@ -53,17 +59,18 @@ Colocando courses, vou ter acesso no meu componente dessa forma this.props.cours
 1° state representa o state que está dentro do redux store
 2° ownProps ele da acesso as propriedades desse componente container
 */
-function mapStateToProps(state, ownProps){
-    debugger;
-    return {
-        courses: state.courses, //Vem do reducer courseReducer
-    }
+function mapStateToProps(state, ownProps) {
+  debugger;
+  return {
+    courses: state.courses, //Vem do reducer courseReducer
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        createCourse: course => dispatch(courseActions.createCourse(course))
-    };
+  return {
+    //Desse jeito ele pega totos as minhas actions dentro do courseActions
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage); 
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- pluralsight-redux-starter/src/reducers/courseReducer.js
+++ pluralsight-redux-starter/src/reducers/courseReducer.js
@@ -1,9 +1,9 @@
+import * as types from '../actions/actionTypes';
 //Para não ter erro, o reducer precisa ter um valor padrão, para isso vamos usar um [] inicial
 //Quer dizer que como não temos curso ainda ele fica com o array vazio
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case 'CREATE_COURSE':
-            debugger;
+        case types.CREATE_COURSE:
             //state.push(action.course); Errado precisa ser imutável;
             return [...state, Object.assign({}, action.course)];
         default:
